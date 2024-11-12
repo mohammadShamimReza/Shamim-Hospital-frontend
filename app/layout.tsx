f1809bc4c6321638398233b61e4d609a7b2b9eac
Md Shamim Reza <mohammadshamimreza23393@gmail.com>
2024-11-12 18:50:25 +0600

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
